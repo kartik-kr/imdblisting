9695ebb491767b29c07d7d66f4257de37be76253
kartik kr <kartikganguly@gmail.com>
2017-01-18 12:31:36 +0530

    modified the code for movie listing

--- sourcefiles/jscript.js
+++ sourcefiles/jscript.js
@@ -1,43 +1,36 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
+//intial input of text
+var movies= new XMLHttpRequest();
+movies.open("GET", "https://api.themoviedb.org/3/movie/now_playing?api_key=2521beb8b455d1a0edd6014abca25e6e&language=en-US&page=1",false);
+movies.send(null);
+r=JSON.parse(movies.response);
 var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+var list=[];
+for(i=0;i<r.results.length;i++)
+{
+//alert(r.results[i].title);
+list.push(r.results[i].title);
 }
 
-// Click on a close button to hide the current list item
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+// function to add element
+function add(inputtext){
 
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("newmovie").value;
-  var t = document.createTextNode(inputValue);
+  var t = document.createTextNode(inputtext);
   li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("mylist").appendChild(li);
-  }
-  document.getElementById("newmovie").value = "";
+  document.getElementById("mylist").appendChild(li);
+  return li;
 }
+
+//function to close an element
+function close(li)
+{
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-var close = document.getElementsByClassName("close");
-var i;
+  var close = document.getElementsByClassName("close");
+  var i;
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
@@ -45,3 +38,26 @@ var i;
       div.style.display = "none";
     }
   }
+}
+
+// Create a "close" button and append it to each list item
+var i;
+
+for(i=0;i<list.length;i++)
+{
+  var li=add(list[i]);
+  close(li);
+}
+ 
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var inputValue = document.getElementById("newmovie").value;
+  if (inputValue === '') {
+    alert("No input");
+  } else {
+   var li = add(inputValue);
+   close(li);
+   document.getElementById("newmovie").value = "";
+  }
+}
